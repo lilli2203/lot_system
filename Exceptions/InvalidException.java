93f6892ca5d5a0fe8d259ef50c0dc5a84ea50d87
lilli2203 <mailyourlilli@gmail.com>
2024-07-13 15:33:16 +0530

    updated

--- Exceptions/InvalidException.java
+++ Exceptions/InvalidException.java
@@ -1,7 +1,36 @@
 package com.parking.Exceptions;
 
-public class InvalidException extends Exception{
+public class InvalidException extends Exception {
+    private int errorCode;
+
     public InvalidException(String message) {
         super(message);
+    }
+    public InvalidException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidException(String message, int errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public InvalidException(String message, Throwable cause, int errorCode) {
+        super(message, cause);
+        this.errorCode = errorCode;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+    @Override
+    public String toString() {
+        return "InvalidException{" +
+                "errorCode=" + errorCode +
+                ", message=" + getMessage() +
+                '}';
     }
 }
